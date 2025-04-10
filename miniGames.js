3aedf9f451c344949b166ad873a6526256ff3577
Nikkalas11 <nikolaj.sever11@gmail.com>
2025-04-10 21:47:33 +0300

    Troubleshooting

--- miniGames.js
+++ miniGames.js
@@ -1,16 +1,20 @@
-
-
 function guessNumber() {
-    let randomNumber = Math.floor(Math.random() * 100) + 1;
-    // console.log(randomNumber);
-    let userNumber = Number(prompt(`Попробуйте отгадать число от 1 до 100`));
-    while (randomNumber !== userNumber) {
-       if (userNumber > randomNumber) {
-        userNumber = Number(prompt(`Ваше число ${userNumber} больше`));
-        } 
-         else if (userNumber < randomNumber) {
-        userNumber = Number(prompt(`Ваше число ${userNumber} меньше`));
-        }
+  let randomNumber = Math.floor(Math.random() * 100) + 1;
+  // console.log(randomNumber);
+  let userNumber = prompt(`Попробуйте отгадать число от 1 до 100`);
+  let attempt = 0;
+  while (true) {
+    attempt ++;
+    if (isNaN(userNumber) || !Boolean(userNumber)) {
+      alert(`Вы ввели не правильное значение. Попробуйте снова`);
+      break;
+    } else if (userNumber > randomNumber) {
+      userNumber = prompt(`Ваше число ${userNumber} - больше`);
+    } else if (userNumber < randomNumber) {
+      userNumber = prompt(`Ваше число ${userNumber} - меньше`);
+    } else {
+      alert(`Вы угадали задуманное число ${randomNumber} за ${attempt} попыток`);
+      break;
     }
-    alert(`Вы выиграли! Задуманное число ${randomNumber}`);
+  }
 }
